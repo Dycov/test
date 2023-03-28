6f1a49aa12e133b2a91c2c12dfe6f3ca63e16bfa
Dycov <sndirangu3@gmail.com>
2023-03-28 06:41:43 -0700

    Function0

--- main.h
+++ main.h
@@ -2,43 +2,35 @@
 #define MAIN_H
 
 #include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
 #include <stdarg.h>
 
-/**
- * struct function - structure to find the correct function for a specifier
- * @spec: The specifier in question
- * @funct: The function associated with the specifier
- */
-typedef struct function
-{
-	char *spec;
-	int (*funct)(va_list *);
-} function_t;
-
-int _printf(const char *format, ...);
 int _putchar(char c);
-int _prntchar(va_list *args);
-void putLong(long x);
-int pr_int(va_list *args);
-int print_min(void);
-int pr_uint(va_list *args);
-void pr_uints(unsigned int c);
-int _prnthex(va_list *args);
-int _prntcaphex(va_list *args);
-char *_memcpy(char *dest, char *src, unsigned int n);
-int _prntstr(va_list *args);
-char *decToOctal(int decimalnum);
-int _prntoct(char *a);
-int _prntadd(unsigned a);
-int _prntprct(va_list *args);
-void print_address_hex(void* p0);
-char hex_digit(int v);
-char *_itoa(int n, char *str, int base);
-int _strlen(char *s);
-int _strcmp(char s1, char *s2);
-int (*_get_function(char s))(va_list * args);
-void reverse(char str[], int length);
+int _puts(char *str);
+int _printf(const char *format, ...);
+int print_char(va_list c);
+int print_string(va_list s);
+int print_dec(va_list d);
+int print_hex(va_list x);
+int print_HEX(va_list X);
+int print_octal(va_list o);
+int print_bin(va_list b);
+int print_unsigned(va_list u);
+int print_rot13(va_list R);
+int print_rev(va_list r);
+int print_int(va_list i);
+int print_S(va_list S);
+int print_p(va_list p);
+
+/**
+ * struct code_format - structure format
+ * @sc: specifier
+ * @f: function associated
+ */
+
+typedef struct code_format
+{
+  char *sc;
+  int (*f)(va_list);
+} code_f;
 
 #endif
